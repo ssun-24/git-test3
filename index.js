bbd1e34f89acc40b2e1c4249603318cc9710558e
dudgh867@naver.com <dudgh867@naver.com>
2024-10-29 16:02:04 +0900

    fix semicolon

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function add(a, b) {
-  return a + b
+  return a + b;
 }
 
 function susbtract(a, b) {
-  return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-  return a * b
+  return a * b;
 }
 
 FileSystemfs
